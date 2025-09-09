979d10fcc9c87a428fa361b9cda1a51d23bd865a
Juan Diego <chucjuan508@gmail.com>
2025-09-09 09:47:33 -0800

    Funcion recursiva Suma de N naturales

--- Actividad_24.py
+++ Actividad_24.py
@@ -6,8 +6,14 @@ def factorial(numero):
         return 1
     else:
         return numero * factorial(numero-1)
+
+def suma_n(numero1):
+    if numero1 ==0:
+        return 0
+    else:
+        return numero1+suma_n(numero1-1)
 while True:
-    print("=MENU==")
+    print("MENU")
     print("1. Calcular Factorial")
     print("2. Suma de N numeros")
     print("3. Calcular n-enesimo")
@@ -25,4 +31,6 @@ while True:
 
         case "2":
             print("--Suma N numeros--")
+            numero1 = int(input("Ingrese el numero a calcular: "))
+            print(suma_n(numero1))
 
